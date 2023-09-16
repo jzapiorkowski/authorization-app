e2e42fa03281346f5775aab5e7ffefbf22097bee
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2023-09-16 15:34:57 +0200

    display username on password change

--- apps/client/src/app/user-management/user-management.component.ts
+++ apps/client/src/app/user-management/user-management.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../shared/services/user/user.service';
 
@@ -7,9 +7,10 @@ import { UserService } from '../shared/services/user/user.service';
   templateUrl: './user-management.component.html',
   styleUrls: ['./user-management.component.scss'],
 })
-export class UserManagementComponent {
+export class UserManagementComponent implements OnInit {
   constructor(private userService: UserService) {}
 
+  username!: string;
   form: FormGroup = new FormGroup({
     password: new FormControl('', [Validators.required]),
   });
@@ -21,5 +22,9 @@ export class UserManagementComponent {
 
       this.userService.changePassword(userId, password);
     }
+  }
+
+  ngOnInit(): void {
+    this.username = this.userService.getUserUsername;
   }
 }
