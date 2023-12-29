46f33ccfa47f45297e0fcd90af51f414ee945155
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2023-12-29 17:27:22 +0100

    remove unused component

--- apps/client/src/app/shared/shared.module.ts
+++ apps/client/src/app/shared/shared.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { ServicesModule } from './services/services.module';
-import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
-  imports: [ServicesModule, ComponentsModule],
-  exports: [ServicesModule, ComponentsModule],
+  imports: [ServicesModule],
+  exports: [ServicesModule],
 })
 export class SharedModule {}
